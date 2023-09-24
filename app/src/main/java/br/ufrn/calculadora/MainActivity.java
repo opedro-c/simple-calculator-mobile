8ca8dba0b1fe9aa47e96e3c6151f783f1f441ff8
Pedro Costa <pedroc_aragao@outlook.com>
2023-09-24 16:55:53 -0300

    tabs switching correctly

--- app/src/main/java/br/ufrn/calculadora/MainActivity.java
+++ app/src/main/java/br/ufrn/calculadora/MainActivity.java
@@ -1,14 +1,59 @@
 package br.ufrn.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment fragmentSimpleCalculator = new FragmentSimpleCalculator();
+    Fragment fragmentGradeCalculator = new FragmentGradeCalculator();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0)
+                    switchToSimple();
+                else if (tab.getPosition() == 1)
+                    switchToGrade();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+        initializeFragment();
+    }
+
+    private void initializeFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.fragmentContainerView, fragmentSimpleCalculator);
+        transaction.commit();
+    }
+
+    private void switchToSimple() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainerView, fragmentSimpleCalculator);
+        transaction.commit();
+    }
+
+    private void switchToGrade() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainerView, fragmentGradeCalculator);
+        transaction.commit();
     }
 }
